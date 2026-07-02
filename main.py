14969c18749e1304abe3213257e2da41a1552195
AcharaChisomSolomon <acharachisom861@gmail.com>
2026-07-02 15:36:38 +0100

    use openrouter and create dummy project to test agent on

--- main.py
+++ main.py
@@ -1,5 +1,42 @@
+import argparse
+import os
+from   dotenv import load_dotenv
+from   openai import OpenAI
+
+load_dotenv()
+
+
 def main():
-    print("Hello from ai-agent!")
+    api_key = os.environ.get("OPENROUTER_API_KEY")
+    if api_key is None:
+        raise RuntimeError("OPENROUTER API KEY not found")
+    
+    client = OpenAI(
+        base_url="https://openrouter.ai/api/v1",
+        api_key=api_key,
+    )
+
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str,            help="User prompt")
+    parser.add_argument("--verbose",   action="store_true", help="Enable verbose output")
+    args = parser.parse_args()
+
+    user_prompt = args.user_prompt
+    messages = [
+        {"role": "user", "content": user_prompt},
+    ]
+
+    response = client.chat.completions.create(
+        model="openrouter/free",
+        messages=messages
+    )
+
+    if args.verbose:
+        print(f"User prompt: {user_prompt}")
+        print(f"Prompt tokens: {response.usage.prompt_tokens}")
+        print(f"Response tokens: {response.usage.completion_tokens}")
+    print(f"Response: {response.choices[0].message.content}")
+
 
 
 if __name__ == "__main__":
